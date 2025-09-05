581a14d97fcd40047770dd02d808001bfc1896a3
sourabhjain04 <sourabhjain0412@gmail.com>
2025-09-05 12:41:28 +0530

    To learn Git

---
+++ SpringBootExampleCRUDGIT/src/test/java/com/SpringBootTest/SpringBootExampleCrudgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleCrudgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
